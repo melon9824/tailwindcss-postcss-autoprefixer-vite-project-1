0e9ceb4ef62f97af5a89f9a500b3965d5899f894
khorshed-alam <ka.melon1968@gmail.com>
2023-06-18 02:23:13 +0600

    added scroll section active link

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-
+/**
+ * ! light / dark theme ===
+ */
 const html = document.getElementById("html");
-const btn = document.getElementById("theme-btn");
+const themeBtn1 = document.getElementById("theme-btn-1");
+const themeBtn2 = document.getElementById("theme-btn-2");
 
-btn.addEventListener("click", () => {
+themeBtn1.addEventListener("click", () => {
     html.classList.toggle("dark");
 });
+
+themeBtn2.addEventListener("click", () => {
+    html.classList.toggle("dark");
+});
+
+
+/**
+ * ! scroll section active link ===
+ */
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('nav ul li a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
