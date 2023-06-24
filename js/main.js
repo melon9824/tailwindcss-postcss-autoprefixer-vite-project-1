e1c8707351068ee6ea9c463e6c358ac6d02b2b53
khorshed-alam <ka.melon1968@gmail.com>
2023-06-24 17:53:29 +0600

    pricing section added

--- js/main.js
+++ js/main.js
@@ -35,3 +35,29 @@ window.onscroll = () => {
         };
     });
 };
+
+
+/**
+ * ! pricing
+ */
+const toggleBtn = document.getElementById('toggleBtn');
+
+const card_1_front = document.querySelector('#card_1_front');
+const card_1_back = document.querySelector('#card_1_back');
+
+const card_2_front = document.querySelector('#card_2_front');
+const card_2_back = document.querySelector('#card_2_back');
+
+const card_3_front = document.querySelector('#card_3_front');
+const card_3_back = document.querySelector('#card_3_back');
+
+toggleBtn.addEventListener('change', () => {
+    card_1_front.classList.toggle('-rotate-y-180');
+    card_1_back.classList.toggle('rotate-y-180');
+
+    card_2_front.classList.toggle('-rotate-y-180');
+    card_2_back.classList.toggle('rotate-y-180');
+
+    card_3_front.classList.toggle('-rotate-y-180');
+    card_3_back.classList.toggle('rotate-y-180');
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const rotateY = plugin(function ({addUtilities}) {
+  addUtilities ({
+    ".rotate-y-180": {
+      transform: "rotateY(180deg)"
+    },
+    ".-rotate-y-180": {
+      transform: "rotateY(-180deg)"
+    }
+  });
+})
+
+
 module.exports = {
   darkMode: "class",
   content: ["*"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.25rem",
+        md: "3.125rem"
+      }
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [rotateY],
 }
 
